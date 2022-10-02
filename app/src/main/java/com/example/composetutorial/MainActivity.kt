e3226810dd94052a730fe64e15530aaf9153012c
Khubaib <mohmmedkhubaib2001@gmail.com>
2022-10-02 19:57:11 +0530

    state in compose

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composetutorial.ui.theme.ComposeTutorialTheme
@@ -29,27 +29,39 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun MyApp() {
+fun MyApp(names:List<String> = listOf("world","Compose")) {
     // A surface container using the 'background' color from the theme
     Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-        Greeting("Khubaib")
+        Column(modifier = Modifier.padding()) {
+            for (name in names) {
+                Greeting(name)
+            }
+        }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary) {
-        Column {
-            Text(
-                text = "Hello,",
-                modifier = Modifier.padding(24.dp)
-            )
-            Text(
-                text = name,
-                modifier = Modifier.padding(24.dp)
-            )
+    val expanded = remember {
+        mutableStateOf(false)
+    }
+    val extraPadding = if(expanded.value) 48.dp else 0.dp
+    Surface(color = MaterialTheme.colors.primary,
+    modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
+        ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+            OutlinedButton(onClick = { expanded.value = !expanded.value}) {
+                Text(if(expanded.value) "Show Less" else "Show More")
+            }
         }
-
     }
 }
 
